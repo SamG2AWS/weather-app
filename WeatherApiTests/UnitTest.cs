3d6b3e58cfc11b4c5a58a07918a766db1f4adca1
Samir Gurav <64665867+SamG2AWS@users.noreply.github.com>
2020-11-18 01:48:44 -0500

    Added a new unit test project

---
+++ WeatherApiTests/UnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WeatherApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
