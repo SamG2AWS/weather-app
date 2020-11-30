5d6f98dab542af1fe1669526b649ba36bd6e61b0
Samir Gurav <64665867+SamG2AWS@users.noreply.github.com>
2020-11-30 00:44:26 -0500

    Upload IIS deployment powershell file

---
+++ IISDeployment.ps1
@@ -0,0 +1,29 @@
+Import-Module WebAdministration
+
+$iisAppPoolName = "WeatherApi-AppPool"
+$iisAppName = "WeatherApi"
+$directoryPath = "D:\\Jenkins\\WeatherApi"
+
+
+#navigate to the app pools root
+cd IIS:\AppPools\
+
+#check if the app pool exists
+if (!(Test-Path $iisAppPoolName -pathType container))
+{
+    #create the app pool
+    $appPool = New-Item $iisAppPoolName
+}
+
+#navigate to the sites root
+cd IIS:\Sites\
+
+#check if the site exists
+if (Test-Path $iisAppName -pathType container)
+{
+    return
+}
+
+#create the site
+$iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation=":90:" + $iisAppName} -physicalPath $directoryPath
+$iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName
