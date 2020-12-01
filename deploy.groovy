6531404bdf9582b738840588bfb10e3ac02ed434
Samir Gurav <64665867+SamG2AWS@users.noreply.github.com>
2020-12-01 01:39:36 -0500

    Assign correct permissions

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,6 @@
 def deployProject(){
     powershell 'Copy-Item -Path "WeatherApi\\bin\\Release\\net5.0\\publish\\*" -Destination "D:\\Jenkins\\WeatherApi" -Recurse -Force'
+    powershell returnStatus: true, script: '.\\AssignPermissions.ps1'
     powershell returnStatus: true, script: '.\\IISDeployment.ps1'
 
 }
